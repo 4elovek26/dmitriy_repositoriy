43a89261950a0aa9149c3f44fecc62418d4b3dce
4elovek26 <94823832+4elovek26@users.noreply.github.com>
2021-12-03 17:50:03 +0300

    изучение php, html, css, postgresql

--- index.php
+++ index.php
@@ -1,143 +1,36 @@
-<!DOCTYPE HTML>
-<html lang = "en">
-<head>
-    <meta chgarset=<"UTF-8">
-    <title>4elovek</title>
-</head>
-<body>
 <?php
+require_once 'setting.php';
 
-class statuser {
-    public static $name;
-    public static function hello(){
-        echo " Hello ".self::$name."<br>";
-    }
+//$conn = new PDO($dsn);
+$conn = pg_connect("host={$host} port=5432 dbname={$db} user={$username} password={$password}");
+if($conn){
+    echo "Connected to the <strong>$db</strong> database successfully! <br>";
 }
 
-statuser::$name = "Alex";
-echo statuser::$name;
-statuser::hello();
+$name = "Dimon";
+$result = pg_query($conn, "Select * from Users");
+$rows = pg_num_rows($result);
 
-Class ShoppingCard {
-//пустой
+for ($i = 0; $i < $rows; $i++) {
+    pg_result_seek($result, $i);
+    $onerow = pg_fetch_assoc($result);
+    echo 'Id: ' . $onerow['id_user'] . ' ';
+    echo 'Name : ' . $onerow['name'] . '<br>';
 }
 
-Final Class Shop {
-    Private $name;
-
-    final public function naming() {
-        $this -> name = "Adidas";
-        echo $this->name."<br>";
-    }
-}
-
-Class User {
-   /* public $name = "Дима";
-    public $password = "123q";
-    public $email = "qw@mail.ru";
-    public $city = "Сыктывкар"; появление __construct*/ 
-
-    private static $nameteach4;
-
-    function __construct($name, $password, $email, $city)
-    {
-        $this->name = $name;
-        $this->password = $password;
-        $this->email = $email;
-        $this->city = $city;
-    }
-
-    function __destruct()
-    {
-        echo "<br> Это сообщение выводится после уничтожения класса {$this->name}";
-    }
-
-    public static function setNameteach4($name1) {
-        self::$nameteach4 = $name1;
-    }
-
-    public static function getNameteach4(){
-        return self::$nameteach4;
-    }
-
-    public function Hello() {
-        echo "Hello {$this->name}";
-    }
-
-    function getInfo(){
-        $information = "<br>{$this->name}"." ". "{$this->surname} <br>";
-        return $information;
-    }
-
-
-
-    public function getName() {
-        echo $this->name;
-        $this->test();
-    }
-
-    public function test() {
-        echo " Test <br>";
-    }
-
-
+$test_query = pg_prepare($conn, "test_q", "Select * from users where name = $1");
+$test_query = pg_execute ($conn, "test_q", array($name));
+$rows = pg_num_rows($test_query);
+for ($i = 0; $i < $rows; $i++) {
+    pg_result_seek($test_query, $i);
+    $onerow = pg_fetch_assoc($test_query);
+    echo 'Id: ' . $onerow['id_user'] . ' ';
+    echo 'Name : ' . $onerow['name'] . '<br>';
 }
 
 
-class Moderator extends User {
+pg_close($conn);
 
-    function __construct($name, $password, $email, $city, $info, $rights)
-    {
-        parent::__construct($name, $password, $email, $city);
-        $this->info = $info;
-        $this->rights = $rights;
-    }
-   
-    public function privetstvie() {
-        echo "Moderator is here";
-    }
-
-    function getInfo(){
-        $information = parent::getInfo();
-        $information .= "{$this->info}"." ". "{$this->rights} <br>";
-        return $information;
-    }
- 
-}
-
-
-
-$product1 = new ShoppingCard();
-$product = new Shop;
-$admin = new User("Дмитрий", "123q", "qw@mail.ru", "Сыктывкар");
-$user1 = new User("1", "2", "3", "4");
-
-var_dump($product1 instanceof ShoppingCard);
-
-echo $admin->name."<br>";
-//$user1->name = "Андрей"; появление __construct
-$user1->surname = "Иванов"; 
-
-echo $user1->surname." ".$user1->name."<br>";
-$product->naming();
-//$product->name = "Nike"; //ошибка, переменная name = private
-
-//$admin->name = "Алексей"; появление __construct
-$admin->surname = "Имнадзе"; 
-echo "{$admin->Hello()} <br>";
-echo "Пользователь: {$admin->getInfo()}";
-var_dump($user1);
-echo "<br>";
-$admin->name = "Димон";
-$admin->getName();
-
-
-User::setNameteach4('Ivan');
-echo User::getNameteach4()."<br>";
-
-$moder = new Moderator("ModerName", "ModerPass", "ModerEmail", "ModerPhone", "Moderator", "True");
-echo $moder->getInfo();
-echo $moder->privetstvie();
 
 ?>
 
